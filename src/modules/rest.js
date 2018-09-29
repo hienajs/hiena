c6f32c1e0fe4e5693ed3516dc0482fed5c97582f
Raul Silva Bunicenha <raulbunicenha@gmail.com.br>
2018-09-29 15:33:45 -0300

    Alterações de estrutura

--- src/modules/rest.js
+++ src/modules/rest.js
@@ -15,64 +15,61 @@ async function compose (methods, req, res, mixins) {
   return data
 }
 
-function expCompose (config, req, res, mixins, ...methods) {
-  compose(methods, req, res, mixins)
-    .then(data => {
-      addLog(config, req, true, data)
-      res.status(200).json(data)
-    })
-    .catch(err => {
-      addLog(config, req, false, err.toString())
-      res.status(500).json({ message: err.message })
-    })
+async function expCompose (config, req, res, mixins, ...methods) {
+  try {
+    let data = await compose(methods, req, res, mixins)
+    addLog(config, req, true, data)
+    return res.status(200).json(data)
+  } catch (err) {
+    addLog(config, req, false, err.toString())
+    return res.status(500).json({ message: err.message })
+  }
 }
 
-function composeDownload (config, req, res, mixins, ...methods) {
-  compose(methods, req, res, mixins)
-    .then(data => {
-      if (!data.file) return res.status(500).json({ message: 'File is missing!' })
-      if (!data.name) return res.status(500).json({ message: 'Name is missing!' })
-      res.download(data.file, data.name)
-    })
-    .catch(err => {
-      addLog(config, req, false, err.toString())
-      res.status(500).json({ message: err.message })
-    })
+async function composeDownload (config, req, res, mixins, ...methods) {
+  try {
+    let data = await compose(methods, req, res, mixins)
+    if (!data.file) return res.status(500).json({ message: 'File is missing!' })
+    if (!data.name) return res.status(500).json({ message: 'Name is missing!' })
+    return res.download(data.file, data.name)
+  } catch (err) {
+    addLog(config, req, false, err.toString())
+    return res.status(500).json({ message: err.message })
+  }
 }
 
-function composeContent (config, req, res, mixins, ...methods) {
-  compose(methods, req, res, mixins)
-    .then(data => {
-      if (!data.content) return res.status(500).json({ message: 'Content is missing!' })
-      res.contentType(data.content)
-      if (!data.value) return res.status(500).json({ message: 'Value is missing!' })
-      res.send(data.value)
-    })
-    .catch(err => {
-      addLog(config, req, false, err.toString())
-      res.status(500).json({ message: err.message })
-    })
+async function composeContent (config, req, res, mixins, ...methods) {
+  try {
+    let data = await compose(methods, req, res, mixins)
+    if (!data.content) return res.status(500).json({ message: 'Content is missing!' })
+    res.contentType(data.content)
+    if (!data.value) return res.status(500).json({ message: 'Value is missing!' })
+    return res.send(data.value)
+  } catch (err) {
+    addLog(config, req, false, err.toString())
+    return res.status(500).json({ message: err.message })
+  }
 }
 
 export function rest (app, mixins, config) {
   return {
     get (path, ...methods) {
-      app.get(path, (req, res) => expCompose(config, req, res, mixins, ...methods))
+      app.get(path, async (req, res) => expCompose(config, req, res, mixins, ...methods))
     },
     getDownload (path, ...methods) {
-      app.get(path, (req, res) => composeDownload(config, req, res, mixins, ...methods))
+      app.get(path, async (req, res) => composeDownload(config, req, res, mixins, ...methods))
     },
     getContent (path, ...methods) {
-      app.get(path, (req, res) => composeContent(config, req, res, mixins, ...methods))
+      app.get(path, async (req, res) => composeContent(config, req, res, mixins, ...methods))
     },
     post (path, ...methods) {
-      app.post(path, (req, res) => expCompose(config, req, res, mixins, ...methods))
+      app.post(path, async (req, res) => expCompose(config, req, res, mixins, ...methods))
     },
     put (path, ...methods) {
-      app.put(path, (req, res) => expCompose(config, req, res, mixins, ...methods))
+      app.put(path, async (req, res) => expCompose(config, req, res, mixins, ...methods))
     },
     delete (path, ...methods) {
-      app.delete(path, (req, res) => expCompose(config, req, res, mixins, ...methods))
+      app.delete(path, async (req, res) => expCompose(config, req, res, mixins, ...methods))
     }
   }
 }
